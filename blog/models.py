1a941b66745a5d558fd215e0b714e59f814f68e8
Amanda Flynn <amandaflynn98@gmail.com>
2025-01-10 12:04:26 +0000

    metadata updated to post model

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self): return f"The title of the post is {self.title} | written by {self.author}"
 
 class Comment (models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
