1fac0d7dafc965c0e0d9bdea3b3727e816508863
Amanda Flynn <amandaflynn98@gmail.com>
2025-01-12 14:58:08 +0000

    dont know if this is a mistake, migrations made and fixed post meta.

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     class Meta:
         ordering = ["-created_on"]
-    def __str__(self): return f"The title of the post is {self.title} | written by {self.author}"
+    def __str__(self): return f"{self.title} | written by {self.author}"
 
 class Comment (models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
